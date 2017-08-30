a58842cd8d8cb927bf72304b5bc07e329c0d135a
Angelo <Angelo@PC-ANGELO>
2017-08-30 03:48:16 +0200

    Added fromTagToHexStringLong

--- src/Run.java
+++ src/Run.java
@@ -1,20 +1,26 @@
 public class Run {
 
 	public static void main(String[] args) {
-		String accIdHex = fromTagToHexString("PQ9090GJ");
+		String accIdHex = fromTagToHexStringLong("2GJY9990");
+		System.out.println(accIdHex);
 		byte[] payload = SCUtils.hexToBuffer(accIdHex);
 		BuffParser bp = new BuffParser(payload, 0);
-		System.out.println("My tag with RRSLONG is: " + SCUtils.tagFromId(bp.readRrsLong()));
-		//System.out.println("My tag with SLONG is: " + SCUtils.tagFromId(bp.readLong()) + "\n");
+		//System.out.println("My tag with RRSLONG is: " + SCUtils.tagFromId(bp.readRrsLong()));
+		System.out.println("My tag with SLONG is: " + SCUtils.tagFromId(bp.readLong()) + "\n");
 		System.out.println(bp.toDiscordResponse());
 	}
 
-	public static String fromTagToHexString(String tag) {
+	public static String fromTagToHexStringRrsLong(String tag) {
 		BuffParser.SLong accId = SCUtils.idFromTag(tag);
 		byte[] hiBytes = SCUtils.intToRrsInt32(accId.hi);
 		byte[] loBytes = SCUtils.intToRrsInt32(accId.lo);
 		String accIdHex = SCUtils.toHexString(hiBytes) + SCUtils.toHexString(loBytes);
 		return accIdHex;
 	}
+	
+	public static String fromTagToHexStringLong(String tag) {
+		BuffParser.SLong accId = SCUtils.idFromTag(tag);
+		return String.format("%08X", accId.hi) + String.format("%08X", accId.lo);
+ 	}
 
 }
